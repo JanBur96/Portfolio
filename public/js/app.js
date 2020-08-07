f88b1865b012fa74872bf06af9493299459b9ae7
JanBur96 <jan.burghardt1706@icloud.com>
2020-08-07 12:51:10 +0200

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+// Get the elements
+let upButton = document.getElementById('button-up');
+
+// Scroll to top button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    upButton.style.display = "block";
+  } else {
+    upButton.style.display = "none";
+  }
+}
+
+upButton.addEventListener('click', topFunction)
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
