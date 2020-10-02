d354a1222dc7e129aa81c554b638527745855e83
JanBur96 <jan.burghardt1706@icloud.com>
2020-10-02 12:04:01 +0200

    updating stuff

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,22 @@
 let upButton = document.querySelector('.button-up');
 
 // Scroll to top button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    upButton.style.display = "block";
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    upButton.style.display = 'block';
   } else {
-    upButton.style.display = "none";
+    upButton.style.display = 'none';
   }
 }
 
-upButton.addEventListener('click', topFunction)
+upButton.addEventListener('click', topFunction);
 
 function topFunction() {
   document.documentElement.scrollTop = 0;
